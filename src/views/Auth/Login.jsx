0f5cd072fd8d0aae84d9fd103865f5fbc36fc9d3
Cole Rossman <colerossman99@gmail.com>
2022-05-04 12:39:27 -0700

    tests pass

--- src/views/Auth/Login.jsx
+++ src/views/Auth/Login.jsx
@@ -26,23 +26,32 @@ export default function Login() {
     // from React Router to replace the current URL with the URL
     // we need to redirect to.
     // See https://v5.reactrouter.com/web/api/history for the appropriate method to use
+    if (loginWasSuccessful) {
+      history.replace(from);
+    } else {
+      setError('Login was unsuccessful, please try again');
+    }
   };
 
   return (
     <>
       <h3>You must log in to view the page at {from.pathname}</h3>
       <form onSubmit={handleLogin} className={styles.loginForm}>
-        <label>Email</label>
+        <label htmlFor='email'>Email</label>
         <input
           id="email"
           name="email"
           type="email"
+          onChange={handleFormChange}
+          value={formState.email}
         />{' '}
-        <label>Password</label>
+        <label htmlFor='password'>Password</label>
         <input
           id="password"
           name="password"
           type="password"
+          onChange={handleFormChange} 
+          value={formState.password}
         />
         <button type="submit" aria-label="Sign In">
           Sign in
